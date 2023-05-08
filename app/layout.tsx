cf14ad3b198238481fd285414052b217380d01f9
andresdavidsv <soy@andresdavidsv.com>
2023-05-08 12:59:59 -0500

    feat: continue with navbar, add modal to show register form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from 'next/font/google';
 import './globals.css';
 import { Navbar } from './components/Navbar';
+import { ClientOnly } from './utils/ClientOnly';
+import { Modal } from './components/Modal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+          <Modal isOpen />
+        </ClientOnly>
         {children}
       </body>
     </html>
