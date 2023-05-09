ca7c70bdb3cf4ae00a8f122e3e34a5876b26dae9
andresdavidsv <soy@andresdavidsv.com>
2023-05-08 21:52:03 -0500

    feat: continue modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <Navbar />
-          <Modal isOpen />
+          <Modal isOpen title="Hello World" actionLabel="Submit" />
         </ClientOnly>
         {children}
       </body>
